2a70726390ff361cd4eaed7b8e2d964b0c975782
Mshanga Makonde <41159327+mshanga@users.noreply.github.com>
2023-04-26 12:50:55 +0300

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-// script that prints the title of a Star Wars movie where the e9pisode number matches a given integer.
 
 const req = require('request');
-const episode = process.argv[2];
-const url = 'http://swapi.co/api/films/' + episode;
-
-req(url, function (error, response, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const id = process.argv[2];
+req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
   } else {
